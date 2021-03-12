15a13f70047334acd868cf5a58377eef3db1834c
ChrisRothove <vacantorange@gmail.com>
2021-03-12 11:56:41 -0600

    Updates

---
+++ precache-manifest.722e336affc699fc98aaa6f85644498a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6877a1f85a549d332c4a0de0919653",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "c1b784be10749e25a257",
+    "url": "/pomodoro-timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "223b709c9b9c0b308480",
+    "url": "/pomodoro-timer/static/js/2.dcb25545.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro-timer/static/js/2.dcb25545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b784be10749e25a257",
+    "url": "/pomodoro-timer/static/js/main.81b8c926.chunk.js"
+  },
+  {
+    "revision": "5b934f73dc3f50a7b525",
+    "url": "/pomodoro-timer/static/js/runtime-main.cb3a558a.js"
+  }
+]);
